6524580b8341bda8015711f36a5fc37793000615
Demo <softmate.suport@gmail.com>
2018-12-20 18:00:36 +0530

    first commit new newwwwwww

--- app/src/main/java/com/newapp/MainActivity.java
+++ app/src/main/java/com/newapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView gh;
+    TextView grerh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
