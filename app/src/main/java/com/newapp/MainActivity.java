c9ae1566a4f1aed6c6c77c6aac8435ec7b0c5d38
Demo <softmate.suport@gmail.com>
2018-12-20 15:49:49 +0530

    first commit lineddff

--- app/src/main/java/com/newapp/MainActivity.java
+++ app/src/main/java/com/newapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t11;
+    TextView W11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
